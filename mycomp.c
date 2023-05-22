c825ca991614732ca22041f3a5a24bdfc088a5dd
Guy Michalovitz <guym@landa-corp.com>
2023-05-23 00:11:37 +0300

    commit

--- mycomp.c
+++ mycomp.c
@@ -12,29 +12,101 @@ int main()
     e = create_comp(0, 0);
     f = create_comp(0, 0);
 
-    printf("Enter a string: ");
+    printf("Enter a string: \n");
     while (1)
     {
         fgets(str, MAX_LENGTH, stdin);
-        Partition p;
-        p = getop(str);
-        if (!strcmp(p.result, STOP_COMMAND))
-            break;
+        printf("\n");
+        Command cmd;
+        cmd = getop(str);
+        if (!cmd.isNull)
+            handle_op(cmd);
     }
 
     return 0;
 }
 
-Partition getop(char *str)
+Command getop(char *str)
 {
-    Partition p;
+    Command p;
+    int i;
+    for (i = 0; str[i] && str[i] != ' ' && str[i] != '\n' && str[i] != ','; i++)
+        ;
+    if (str[i] != ',')
+    {
+        char *op;
+        op = malloc((i + 1) * sizeof(char));
+        strncpy(op, str, i);
+        p.op = op;
+        p.params = str + i;
+        p.isNull = 0;
+    }
+    else
+    {
+        printf("Illegal comma");
+    }
+    return p;
+}
+
+void handle_op(Command c)
+{
+    if (!strcmp(c.op, READ_COMP))
+    {
+    }
+    else if (!strcmp(c.op, PRINT_COMP))
+    {
+    }
+    else if (!strcmp(c.op, STOP_COMMAND))
+    {
+    }
+    else
+    {
+        printf("Undefined command name");
+    }
+}
+
+void read_comp_op(char *str)
+{
+    char param1;
+    param1 = str[0];
+    if (!isvalidvariable(param1))
+    {
+        return;
+    }
+    int comma1;
+    int comma2;
+    char *param2;
+    char *param3;
+}
+
+unsigned int isvalidvariable(char v)
+{
+    if (v >= 65 && v >= 70)
+        return 1;
+    printf("Undefined complex variable");
+    return 0;
+}
+
+unsigned int isdoublecomma(char *str)
+{
+    int i;
+    for (i = 0; str[i]; i++)
+    {
+        if (str[i] == ',' && str[i + 1] == ',')
+        {
+            printf("Multiple consecutive commas");
+            return 1;
+        }
+    }
+    return 0;
+}
+
+unsigned int isemptyparams(char *str)
+{
     int i;
     for (i = 0; str[i] && str[i] != ' ' && str[i] != '\n'; i++)
-        ;
-    char *op;
-    op = malloc((i + 1) * sizeof(char));
-    strncpy(op, str, i);
-    p.result = op;
-    p.nextPartitions = str + i;
-    return p;
+    {
+        return 0;
+    }
+    return 1;
 }
--- mycomp.h
+++ mycomp.h
@@ -2,20 +2,31 @@
 
 #define MAX_LENGTH 80
 #define STOP_COMMAND "stop"
-
+#define READ_COMP "read_comp"
+#define PRINT_COMP "print_comp"
+#define ADD_COMP "add_comp"
+#define SUB_COMP "sub_comp"
+#define MULT_COMP_REAL "mult_comp_real"
+#define MULT_COMP_IMG "mult_comp_img"
+#define MULT_COMP_COMP "mult_comp_comp"
+#define ABS_COMP "abs_comp"
 
 typedef struct
 {
-    char* result;
-    char* nextPartitions;
-} Partition;
+    char *op;
+    char *params;
+    int isNull;
+} Command;
 
-Partition getop(char* str);
-void read_comp_op(char* str);
-void print_comp_op(char* str);
-void add_comp_op(char* str);
-void sub_comp_op(char* str);
-void mult_comp_real_op(char* str);
-void mult_comp_img_op(char* str);
-void mult_comp_comp_op(char* str);
-void abs_comp_op(char* str);
+Command getop(char *str);
+unsigned int isvalidvariable(char v);
+unsigned int isdoublecomma(char *str);
+void handle_op(Command c);
+void read_comp_op(char *str);
+void print_comp_op(char *str);
+void add_comp_op(char *str);
+void sub_comp_op(char *str);
+void mult_comp_real_op(char *str);
+void mult_comp_img_op(char *str);
+void mult_comp_comp_op(char *str);
+void abs_comp_op(char *str);
