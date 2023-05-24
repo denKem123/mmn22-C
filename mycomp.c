eae774cf6654ae9be08dabd5b84f2945b669c0a9
Guy Michalovitz <guym@landa-corp.com>
2023-05-24 14:52:41 +0300

    commit

--- mycomp.c
+++ mycomp.c
@@ -20,8 +20,11 @@ int main()
         {
             Command cmd;
             printf("\n%s", str);
-            if (cmd.isNotNull)
+            if (cmd.op != NULL)
+            {
                 free(cmd.op);
+                cmd.op = NULL;
+            }
             cmd = getop(trimString(str));
             if (cmd.isNotNull && handle_op(cmd))
                 break;
